f0636d62904de4d6628886a4a51c1aae66979dbe
Victor Hugo Souza <127329074+victorhbfsouza@users.noreply.github.com>
2023-05-22 21:02:49 -0300

    22/005/2023

---
+++ biblioteca/src/main/java/com/residencia/biblioteca/services/EmailService.java
@@ -0,0 +1,48 @@
+package com.residencia.biblioteca.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+	@Autowired
+	public JavaMailSender emailSender;
+	
+	@Value("${spring.mail.host}")
+	private String mailHost;
+	
+	@Value("${spring.mail.port}")
+	private String mailPort;
+	
+	@Value("${spring.mail.username}")
+	private String userName;
+	
+	@Value("${mail.from}")
+	private String password;
+	
+	@Value("${spring.mail.host}")
+	private String mailFrom;
+	
+	public EmailService(JavaMailSender javaMailSender) {
+		this.emailSender = javaMailSender;
+	}
+	
+	public void enviarEmail (String destinatario, String assunto, String mensagem) {
+		var mailMessage = new SimpleMailMessage ();
+		
+		mailMessage.setTo(destinatario);
+		mailMessage.setSubject (assunto);
+		mailMessage.setText(mensagem);
+		mailMessage.setFrom(mailFrom);
+		
+		try {
+			emailSender.send(mailMessage);
+		} catch(Exception ex) {
+			System.out.println("Ocorreu um erro ao tentar enviar o e-mail."
+						+ex.getMessage());
+		}
+			
+	}
+}
